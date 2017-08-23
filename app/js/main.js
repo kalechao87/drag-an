6ae0d1b810adf61583a85c3965263052366b070b
kalechao87 <kalechao@outlook.com>
2017-08-23 14:57:57 +0800

    drag layer

--- app/js/main.js
+++ app/js/main.js
@@ -33,9 +33,11 @@ function initViewDom() {
     deviceWidth = window.screen.width, // 设备的宽度
     devicePixelRatio = window.devicePixelRatio || 1, // 物理像素和设备独立像素，默认为1
     targetDensitydpi;
+
+    var supportsOrientationChange = (typeof window.orientation == "number" && typeof window.onorientationchange == "object"); 
   
-    var supportsOrientationChange = "onorientationchange" in window,
-      orientationEvent = supportsOrientationChange ? "onorientationchange" : "resize";
+    // var supportsOrientationChange = "onorientationchange" in window;
+    var orientationEvent = supportsOrientationChange ? "onorientationchange" : "resize";
 
     var deviceInfo = 'ua is: ' + ua + ', deviceWidth is: ' + deviceWidth + ', devicePixelRatio is: ' + devicePixelRatio;
     
@@ -67,7 +69,8 @@ function initViewDom() {
         });
       }
 
-      if (window.orientation == 90 || window.orientation == -90) {
+      if (window.orientation
+         == 90 || window.orientation == -90) {
         // alert("横屏");
         var domwidth = document.documentElement.clientWidth;
         var domheight = document.documentElement.clientHeight;
@@ -81,11 +84,39 @@ function initViewDom() {
         });
       }
 
-      $("#log").html(domwidth + "--" + domheight + "--" + scale);
+      $("#log").html("domwidth: " + domwidth + "--domheight: " + domheight + "--scale: " + scale + "--orientationEvent: " + orientationEvent);
       
     }
 }
 
-initViewDom();
+/*
+*when DOMContentLoaded, exec initView
+*/
+(function () {
+  var initView = function () {
+    initViewDom();
+  }
+  document.addEventListener("DOMContentLoaded", initView);
+})();
 
 /*=====  End of initView  ======*/
+
+
+/*----------  draggale  ----------*/
+var dragDis = 0;
+Draggable.create('#drag-layer', {
+  type: 'x',
+  bounds: document.getElementById('drag-scene'),
+  throwProps: false,
+  onDrag: function () {
+    console.log('x is: ' + this.x);
+    dragDis = Math.abs(this.x);
+    console.log("dragDis is: " + dragDis);
+    updateSceneScroll();
+  }
+});
+
+function updateSceneScroll() {
+  $("#main-scene").scrollLeft(dragDis);
+}
+
